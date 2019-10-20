707f6ca2e9d5a8af6d7f71ba8f025ce4ce19d375
EC2 Default User <ec2-user@ip-172-31-20-186.eu-west-1.compute.internal>
2019-10-20 12:22:56 +0000

    initial commit

---
+++ scripts/calc.js
@@ -0,0 +1,19 @@
+Calculator = function() {
+  this.value = 0;  
+};
+
+Calculator.prototype.add = function(number) {
+    if(typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("ERROR!");
+    }
+};
+
+/*function addition(numOne, numTwo) {
+    if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
+        return numOne + numTwo;
+    }
+    else
+        alert("ERROR!");
+}*/
